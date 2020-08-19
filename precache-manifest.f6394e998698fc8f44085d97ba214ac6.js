07d1d5e05120f5e64c840a92e793c8808e8e782c
Crystal Qin <clqin@usc.edu>
2020-08-18 23:16:53 -0700

    Updates

---
+++ precache-manifest.f6394e998698fc8f44085d97ba214ac6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40204fc5e9b171ef64a83fff6f4dfb36",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "18f494c91415a8d1d071",
+    "url": "/react-deploy/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "434628759563955ea336",
+    "url": "/react-deploy/static/css/main.4bf37907.chunk.css"
+  },
+  {
+    "revision": "18f494c91415a8d1d071",
+    "url": "/react-deploy/static/js/2.6ae8cc3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.6ae8cc3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434628759563955ea336",
+    "url": "/react-deploy/static/js/main.f7412cc9.chunk.js"
+  },
+  {
+    "revision": "e62d3fa649f3ef5ab31d",
+    "url": "/react-deploy/static/js/runtime-main.22474196.js"
+  },
+  {
+    "revision": "96f979881ded7760b6e3236f96c56a7b",
+    "url": "/react-deploy/static/media/night5.96f97988.PNG"
+  },
+  {
+    "revision": "293959e1bd7bffefcb6b916b2b5108bf",
+    "url": "/react-deploy/static/media/night6.293959e1.PNG"
+  }
+]);
