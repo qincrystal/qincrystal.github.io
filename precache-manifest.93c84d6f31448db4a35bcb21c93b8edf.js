aa77c6c5d7425e856148da2cdf532ca6c8319721
Crystal Qin <clqin@usc.edu>
2020-09-28 18:51:33 -0700

    Updates

---
+++ precache-manifest.93c84d6f31448db4a35bcb21c93b8edf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4136e0b44ac8639bb7e731e61f85be97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18f494c91415a8d1d071",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "17291f7606327a53aec5",
+    "url": "/static/css/main.d9feb24a.chunk.css"
+  },
+  {
+    "revision": "18f494c91415a8d1d071",
+    "url": "/static/js/2.6ae8cc3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6ae8cc3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17291f7606327a53aec5",
+    "url": "/static/js/main.e7b82fcf.chunk.js"
+  },
+  {
+    "revision": "51ff1df8d02dd786cb92",
+    "url": "/static/js/runtime-main.3ce2ee87.js"
+  },
+  {
+    "revision": "96f979881ded7760b6e3236f96c56a7b",
+    "url": "/static/media/night5.96f97988.PNG"
+  },
+  {
+    "revision": "293959e1bd7bffefcb6b916b2b5108bf",
+    "url": "/static/media/night6.293959e1.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-deploy/precache-manifest.ddeccbe6e16bd60f928ff9ab4df932b4.js"
+  "/precache-manifest.93c84d6f31448db4a35bcb21c93b8edf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-deploy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
